37d7b54bd16b9eb7db73c69458e2153ab1aabbb3
Vincent Lepelley <vincent@lepelley.fr>
2020-04-30 16:42:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const config = require('./app/config')
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
-const serverHttp = app.listen(process.argv[2] || config.port, () => {
+const serverHttp = app.listen(process.env.PORT || process.argv[2] || config.port, () => {
   console.log(chalk.blueBright(`Server listening at http://localhost:${chalk.green(config.port)}`))
   // console.log(`Connected to ${config.database}`)
   console.log('App is running ...')
